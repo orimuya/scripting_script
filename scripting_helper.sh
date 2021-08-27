340c29fd489690762357971489b5782804d64cd0
Dan <madden.danny@gmail.com>
2021-08-27 15:56:14 +0100

    Updating with loops

--- scripting_helper.sh
+++ scripting_helper.sh
@@ -11,13 +11,42 @@ concept=$2
 #Functions containing simple of explanation of concept from args
 bash_if_statement(){
 	echo "Usage:"
-	echo "if [ Test ]"
+	echo "if [ Condition ]"
 	echo "then"
 	echo "<commands>"
 	echo "fi"
-	echo "One liner: if [ Test ]; then <commands>; fi"
-	echo "Accepts logical operators for tests - && AND || OR"
-	echo "e.g. [ Test ] && [ Test ]"
+	echo "One liner: if [ Condition ]; then <commands>; fi"
+	echo "Accepts boolean operations for Conditions - && AND || OR"
+	echo "e.g. [ Condition ] && [ Test ]"
+}
+
+bash_while_loop(){
+	echo "Usage:"
+	echo "while [ Condition ]"
+	echo "do"
+	echo "<commands>"
+	echo "done"
+	echo "One Liner: while [ Condition ]; do <commands>; done"
+	echo "Use : to allow for  an infinite loop"
+	echo "continue returns to start of loop. break exits the loop"
+}
+
+bash_for_loop(){
+	echo "Usage:"
+	echo "for variable in list"
+	echo "do"
+	echo "<commands>"
+	echo "done"
+	echo "One Liner: for variable in list; do <commands>; done"
+	echo "Methods of Listing:"
+	echo "1 2 3 4 5"
+	echo "{Start..End[..Interval]}"
+	echo "\`seq Start End\`"
+
+}
+
+bash_debugging(){
+	echo "Add set -x at start of script"
 }
 
 bash_logical(){
@@ -45,11 +74,23 @@ bash_special_chars(){
 
 python_if_statement(){
 	echo "Usage:"
-	echo "if Test:"
+	echo "if Condition:"
 	echo "\t <commands>"
-	echo "elif Another Test:"
+	echo "elif Another Condition:"
 	echo "\t <commands>"
 	echo "else:"
+	echo "\t <commands>"
+}
+
+python_while_loop(){
+	echo "Usage:"
+	echo "while Condition:"
+	echo "\t <commands>"
+}
+
+python_for_loop(){
+	echo "Usage:"
+	echo "for variable in list:"
 	echo "\t <commands>"
 }
 
