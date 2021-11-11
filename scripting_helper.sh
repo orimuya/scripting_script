5978796c8c84c0bbaef7b81f7b875a7099f62c3a
Dan <madden.danny@gmail.com>
2021-11-11 21:00:18 +0000

    Adding Bash Variables function

--- scripting_helper.sh
+++ scripting_helper.sh
@@ -33,8 +33,7 @@ bash_while_loop(){
 
 bash_for_loop(){
 	echo "Usage:"
-	echo "for variable in list"
-	echo "do"
+	echo "for variable in list; do"
 	echo "<commands>"
 	echo "done"
 	echo "One Liner: for variable in list; do <commands>; done"
@@ -42,6 +41,12 @@ bash_for_loop(){
 	echo "1 2 3 4 5"
 	echo "{Start..End[..Interval]}"
 	echo "\`seq Start End\`"
+	echo "Counter Usage:"
+	echo "for (( expr1 ; expr2 ; expr3 )) ; do"
+	echo "<commands>"
+	echo "done"
+	echo "One Liner: for ((Expr 1 ; Expr 2 ; Expr 3)); do <commands>; done"
+	echo "Classic Example: for ((x=0 ; x<5 ; x++)); do echo "\$x"; done"
 
 }
 
@@ -72,14 +77,24 @@ bash_special_chars(){
 	echo "Special characters are escaped using \\"
 }
 
+bash_variables(){
+	echo "Test if variable is set [ -z \$variable ]"
+	echo "Variable variable can be called \${!var} when var is set to another variable name"
+	echo "Variable can further worked with brackets"
+	echo "Default Value: \${parameter:=defaultValue}"
+	echo "Error Message: \${parameter:?\"Error Message\""
+	echo "Remove: \${var#[#]pattern} - Remove from short(#) or longest(##) pattern"
+	echo "Find/Replace: \${var/pattern/string}"
+}
+
 bash_special_parameters(){
 	echo "\$* Expands to the postional parameters. When quoted expands to a single word seperated by the first character of the IFS variable"
 	echo "\$@ Expands to the postional parameters. When quoted expands to seperate words."
-	echo "\$# Expands to the number of postional parameters as a decimal."
+	echo "\$\# Expands to the number of postional parameters as a decimal."
 	echo "\$? Expands to the exit code of the most recented executed foreground pipe"
 	echo "\$- Exapnds to the current options flags specifed upon invovation, by set or set by the shell"
 	echo "\$\$ Expands to the PID of the shell. In subshell, shows the invoking shell rather than the subshell."
-	echo "\$! Expands to the PID of the most recent backgorund job"
+	echo "\$! Expands to the PID of the most recent background job"
 	echo "\$0 expands to the name of the script"
 }
 
